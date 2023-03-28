be775fd9827ab508cb590219f4f850beef083d52
leejy001 <greatness007@naver.com>
2023-03-28 17:39:33 +0900

    add: 무한 스크롤 구현

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -1,7 +1,9 @@
 import React, { useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
 import { getArticlesInfoApi } from "../api/article";
+import useIntersectionObserver from "../hooks/useIntersectionObserver";
 import { ArticleInfo, ArticlesResult } from "../types/article";
+import { currentTimer } from "../utils/getDateTimeHandler";
 import ListItem from "./ListItem";
 import Spinner from "./Spinner";
 
@@ -10,8 +12,26 @@ interface ParentProps {
 }
 
 function ArticleList({ query }: ParentProps) {
+  const [start, setStart] = useState<number>(0);
   const [articles, setArticles] = useState<ArticleInfo[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [isMore, setIsMore] = useState<boolean>(false);
+
+  const getFirstArticles = useCallback(async () => {
+    let result: ArticlesResult | null = {
+      articles: [],
+      articlesCount: 0
+    };
+    setArticles([]);
+    setLoading(true);
+    result = await getArticlesInfoApi(`${query}limit=${10}&offset=${0}`);
+    if (result?.articles) {
+      setArticles(result?.articles);
+      setStart(10);
+      setLoading(false);
+      setIsMore(result?.articlesCount > 10);
+    }
+  }, [query]);
 
   const getMyArticles = useCallback(async () => {
     let result: ArticlesResult | null = {
@@ -19,33 +39,45 @@ function ArticleList({ query }: ParentProps) {
       articlesCount: 0
     };
     setLoading(true);
-    result = await getArticlesInfoApi(`${query}limit=${10}&offset=${0}`);
+    result = await getArticlesInfoApi(`${query}limit=${10}&offset=${start}`);
     if (result?.articles) {
-      setArticles(result?.articles);
+      setArticles([...articles, ...result?.articles]);
+      setStart(start + 10);
       setLoading(false);
+      setIsMore(result?.articlesCount > start + 10);
     }
-  }, [query]);
+  }, [articles, query, start]);
 
   useEffect(() => {
-    getMyArticles();
-  }, [getMyArticles]);
+    getFirstArticles();
+  }, [getFirstArticles]);
+
+  const onIntersect: IntersectionObserverCallback = ([{ isIntersecting }]) => {
+    if (isIntersecting && isMore && !loading) {
+      getMyArticles();
+    }
+  };
+
+  const { setTarget } = useIntersectionObserver({ onIntersect });
 
   return (
-    <>
-      {loading ? (
-        <Spinner size={50} />
-      ) : (
-        <ArticleListWrapper>
-          {articles.map((item) => (
-            <ListItem key={item.slug} article={item} />
-          ))}
-        </ArticleListWrapper>
-      )}
-    </>
+    <ArticleListContainer>
+      <ArticleListWrapper>
+        {articles.map((item, index) => (
+          <ListItem key={index} article={item} />
+        ))}
+      </ArticleListWrapper>
+      <div ref={setTarget}>{loading && <Spinner size={50} />}</div>
+    </ArticleListContainer>
   );
 }
 
 export default ArticleList;
+
+const ArticleListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const ArticleListWrapper = styled.div`
   display: flex;
--- src/pages/home/components/TagBar.tsx
+++ src/pages/home/components/TagBar.tsx
@@ -8,12 +8,23 @@ interface TagProps {
 }
 
 function TagBar({ setTag }: TagProps) {
+  const [scrollHeight, setScrollHeight] = useState(0);
   const [tags, setTags] = useState<string[]>([]);
+
+  const onScrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   const getTagsInfo = useCallback(async () => {
     const result = await getTagApi();
     if (result?.tags) setTags(result?.tags);
   }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScrollHeight(window.scrollY);
+    });
+  }, [scrollHeight]);
 
   useEffect(() => {
     getTagsInfo();
@@ -36,6 +47,13 @@ function TagBar({ setTag }: TagProps) {
             <Spinner size={30} />
           )}
         </TagListWrapper>
+        <TopButton
+          type="button"
+          onClick={onScrollTop}
+          scroll={scrollHeight > 500}
+        >
+          TOP
+        </TopButton>
       </TagBarWrapper>
     </TagBarContainer>
   );
@@ -46,15 +64,15 @@ export default TagBar;
 const TagBarContainer = styled.div`
   position: relative;
   min-height: 1px;
-  padding-right: 15px;
-  padding-left: 15px;
   flex: 0 0 25%;
   max-width: 25%;
 `;
 
 const TagBarWrapper = styled.div`
+  position: fixed;
   width: 255px;
-  height: 155px;
+  flex: 0 0 25%;
+  max-width: 25%;
   padding: 5px 10px 10px;
   background-color: #f3f3f3;
   border-radius: 5px;
@@ -80,4 +98,18 @@ const TagListWrapper = styled.ul`
     font-size: 14px;
     padding: 0px 8px;
   }
+`;
+
+const TopButton = styled.button<{ scroll: boolean }>`
+display ${({ scroll }) => (scroll ? "block" : "none")};
+  height: 30px;
+  border-radius: 10px;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.COLOR_GRAY};
+  color: ${({ theme }) => theme.colors.FONT_WHITE};
+  cursor: pointer;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 1;
+  margin-top: 10px;
 `;
